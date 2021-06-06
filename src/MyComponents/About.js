2bfb615529427b12186a975b5e295fb9e9abd71d
Nidhi Sanwalia <nidhi.sanwalia@mfine.co>
2021-06-06 16:34:40 +0530

    Todo list added

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component.
+            <p>One of the most important reasons for keeping a to-do list is the organization. Organizing your tasks with a list can make everything much more manageable and make you feel grounded. Seeing a clear outline of your completed and uncompleted tasks will help you feel organized and stay mentally focused.</p>
+        </div>
+    )
+}
